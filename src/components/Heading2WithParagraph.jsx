5928507a148765678c164cbed7516c5ee69f7aa2
En-Chi Liu <enchi.liu@gmail.com>
2021-03-29 16:46:01 +0200

    added Headding2WithParagaph component

---
+++ src/components/Heading2WithParagraph.jsx
@@ -0,0 +1,8 @@
+export default function Heading2WithParagraph({heading, text}) {
+    return(
+        <div className="not-hero">
+            <h2>{heading}</h2>
+            <p>{text}</p>
+        </div>        
+    );
+}
