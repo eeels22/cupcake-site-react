f80747a977755c45537979663b094a115a847579
En-Chi Liu <enchi.liu@gmail.com>
2021-03-29 14:08:13 +0200

    updated App with Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import './styles/App.css';
-import Product from './components/Product';
-import cakesJSON from "./data/cakes.json";
+import "./styles/style.css";
+import Product from "./components/Product";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+//import cakesJSON from "./data/cakes.json";
 import cupcakesJSON from "./data/cupcakes.json";
 
 export default function App() {
   const CupcakesArray = cupcakesJSON.map(item => {
-    return <Product key={item.id} title={item.title} price={item.price} fileName={item.fileName}/>
+    return <Product key={item.id} title={item.title} price={item.price} fileName={item.fileName} alt={item.alt}/>
   });
 
-  const CakesArray = cakesJSON.map(item => {
-    return <Product key={item.id} title={item.title} price={item.price} fileName={item.fileName}/>
+  /*const CakesArray = cakesJSON.map(item => {
+    return <Product key={item.id} title={item.title} price={item.price} fileName={item.fileName} alt={item.alt}/>
   });
+  */
 
-  return (
+  return (           
     <div className="App">
-      <header className="App-header">
-        <Product title="Red velvet" price="30" />
-        <Product title="Chocolate"price="28"/>
-        <Product title="Lemon drizzle"/>
-      </header>
+      <body>
+        <Header/>
+        <div className="cupcake-grid product-grid">
+        {CupcakesArray}
+        </div>
+        
+        <Footer/>   
+      </body>
     </div>
   );
 }
