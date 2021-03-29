d3bfeeaa54ea2c84c52155815d4cc86d8cd7ea57
En-Chi Liu <enchi.liu@gmail.com>
2021-03-29 13:42:18 +0200

    Created Product component based on cupcake

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,19 @@
-// title and fileName are properties of the object props. Without curly braces you need to call props.title etc
-export default function Product({title = "Cupcake", price = 25, fileName}) {
+// title and fileName are properties of the object props.
+// Without curly braces you need to call props.title etc
+// Add default values by passing them in the argument with the assignment operator
+export default function Product({title = "Cupcake", price = 200, fileName, alt}) {
+    const imageObject = require(`../assets/cupcake/${fileName}`);
+    const imageURL = imageObject.default;
+
     return (
-        <article>
-            <h2>{title}</h2>
-            <p>{price}kr</p>
+        <article className="cupcake-item product-item">
+            <img
+              src={imageURL}
+              alt={alt}
+              className="cake-image"
+            />
+            <span className="product-name">{title}</span>
+            <span className="product-price">{price}kr for 6</span>
         </article>
     );
 }
